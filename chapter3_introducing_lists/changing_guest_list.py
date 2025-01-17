fdafc7d74faa446c225ef7cc2119f2e5ccb34a50
mahmoudkoutait <mahmoudkoutait@gmail.com>
2025-01-17 09:46:11 +0200

    edit file sys

--- chapter3_introducing_lists/changing_guest_list.py
+++ chapter3_introducing_lists/changing_guest_list.py
@@ -1,6 +1,6 @@
 # You just heard that one of your guests can’t make the 
-# dinner, so you need to send out a new set of invitations. You’ll have to think of 
-# someone else to invite.
+# dinner, so you need to send out a new set of invitations. You’ll have to think 
+# of someone else to invite.
 #  • Start with your program from Exercise 3-4. Add a print() call at the end of 
 # your program, stating the name of the guest who can’t make it.
 #  • Modify your list, replacing the name of the guest who can’t make it with the 
